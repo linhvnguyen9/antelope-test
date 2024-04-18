f944e41b58ed8f84575b3f0d0a29a29151fe039c
Linh Van Nguyen <linhvnguyen9@gmail.com>
2024-04-18 20:57:44 +0700

    Add account create

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Api, JsonRpc } from 'eosjs';
-import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';  // development only
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';  // DEVELOPMENT ONLY, THIS IS INSECURE!!! https://developers.eos.io/manuals/eosjs/latest/faq/what-is-a-signature-provider
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,23 +15,125 @@ const rpc = new JsonRpc('https://jungle4.cryptolions.io:443'); //required to rea
 const api = new Api({ rpc, signatureProvider }); //required to submit transactions
 
 (async () => {
-  const transaction = await api.transact({
-    actions: [{
+  const buyRamActions = [{
+    account: 'eosio',
+    name: 'buyrambytes',
+    authorization: [{
+      actor: accountName,
+      permission: 'active',
+    }],
+    data: {
+      payer: accountName,
+      receiver: accountName,
+      bytes: 8192,
+    },
+  }];
+
+  const newAccountName = 'erdoxlwldoro';
+  const newAccountOwnerKey = 'EOS6EVrDX2TDGybJNXuHyzz6SpRwmSsL4yC75u5gYnkozeDJav4ok'
+  const newAccountActiveKey = 'EOS7tvPWbq3Q6vRjeaYhHs3iLatmyn2ZZU6GcKNkj77LzJJUBgDqB'
+
+  const createAccountActions = [
+    {
       account: 'eosio',
-      name: 'buyrambytes',
+      name: 'newaccount',
       authorization: [{
         actor: accountName,
         permission: 'active',
       }],
       data: {
-        payer: accountName,
-        receiver: accountName,
-        bytes: 8192,
+        creator: accountName,
+        name: newAccountName,
+        owner: {
+          threshold: 1,
+          keys: [{
+            key: newAccountOwnerKey,
+            weight: 1
+          }],
+          accounts: [],
+          waits: []
+        },
+        active: {
+          threshold: 1,
+          keys: [{
+            key: newAccountActiveKey,
+            weight: 1
+          }],
+          accounts: [],
+          waits: []
+        },
+      }
+    },
+    {
+      account: 'eosio',
+      name: 'ramtransfer',
+      authorization: [{
+        actor: accountName,
+        permission: 'active',
+      }],
+      data: {
+        bytes: 4096,
+        from: accountName,
+        memo: "",
+        to: newAccountName
       },
-    }]
-  }, {
-    blocksBehind: 3,
-    expireSeconds: 30,
-  });
-  console.log(transaction);
+    },
+    // }
+    // {
+    //   account: 'eosio',
+    //   name: 'buyrambytes',
+    //   authorization: [{
+    //     actor: accountName,
+    //     permission: 'active',
+    //   }],
+    //   data: {
+    //     payer: accountName,
+    //     receiver: newAccountName,
+    //     bytes: 2965,
+    //   },
+    // },
+    // {
+    //   account: 'eosio',
+    //   name: 'delegatebw',
+    //   authorization: [{
+    //     actor: accountName,
+    //     permission: 'active',
+    //   }],
+    //   data: {
+    //     from: accountName,
+    //     receiver: newAccountName,
+    //     stake_net_quantity: '1.0000 SYS',
+    //     stake_cpu_quantity: '1.0000 SYS',
+    //     transfer: false,
+    //   }
+    // }
+  ]
+
+  const ramReceiver = 'ekrieidovkdk';
+  const ramTransferActions = [{
+    account: 'eosio',
+    name: 'ramtransfer',
+    authorization: [{
+      actor: accountName,
+      permission: 'active',
+    }],
+    data: {
+      bytes: 4096,
+      from: accountName,
+      memo: "",
+      to: ramReceiver
+    },
+  }]
+
+  // const transaction = await api.transact({
+  //   actions: createAccountActions
+  // }, {
+  //   blocksBehind: 3,
+  //   expireSeconds: 30,
+  // });
+  // console.log(transaction);
+  const accountDetails = await rpc.get_account('erdoxlwldoro')
+  console.log(accountDetails);
+  console.log(accountDetails.permissions[0].required_auth.keys[0].key);
+  console.log(accountDetails.permissions[1].required_auth.keys[0].key);
 })();
